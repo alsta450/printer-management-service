af7d0aa298b6a7e066aeb1e7824802b6180c12bf
Alexander Stadler <a01427369@unet.unive.ac.at>
2025-05-09 07:34:56 +0200

    Setup

---
+++ src/test/java/com/maas/printer_management_service/PrinterManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maas.printer_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
